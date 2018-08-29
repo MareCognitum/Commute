81c6ef85c86f31792362b48d896a9acddcfd5ff5
MareCognitum <MareCognitum@github.com>
2018-08-29 20:48:58 +0200

    minor changes to comments and variable names

--- commute.py
+++ commute.py
@@ -1,19 +1,19 @@
 import googlemaps
 import requests
 import sys
-#declarations
-Token = '' #Enter your Google API Token here
+#Declarations
+token = '' # Enter your Google API Token
 start = sys.argv[1]
 end = sys.argv[2]
 bot_token = '' # Enter your Telegram bot token here
-id = '' # Enter your Telegram chat id here
+chat_id = '' # Enter your Telegram chat id here
 # get Client object
-client = googlemaps.Client(key=Token)
-# get directions
+client = googlemaps.Client(key=token)
+# Get directions
 try:
     directions = client.directions(start,end,alternatives=True)
 
-# prepare data
+# Prepare data
     table = dict()
     for i in range(len(directions)):
         table.setdefault(directions[i]["summary"])
@@ -23,12 +23,12 @@ try:
     for key,value in table.items():
         result_list.append(' '.join([key,':',value, "\n"]))
 
-# get request and send message via bot
+# Get request and send message via bot
     message = ''.join(result_list)
 
-    requests.get('https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(bot_token,id,message))
+    requests.get('https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(bot_token, chat_id, message))
 
 except:
     error = 'Error during processing'
-    requests.get('https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(bot_token, id, error))
+    requests.get('https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(bot_token, chat_id, error))
 
